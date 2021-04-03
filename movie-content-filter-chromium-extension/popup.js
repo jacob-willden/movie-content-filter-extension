51a5575ad8ac26796ca5fe1c7d5b6afe5dd5da86
Jacob Willden <67853915+jacob-willden@users.noreply.github.com>
2021-04-03 00:09:19 -0600

    Script doesn't need to reload anymore

--- movie-content-filter-chromium-extension/popup.js
+++ movie-content-filter-chromium-extension/popup.js
@@ -41,9 +41,9 @@
     you can redistribute it and/or modify it under the terms of the GNU
     General Public License (GNU GPL) as published by the Free Software
     Foundation, either version 3 of the License, or (at your option)
-    any later version.  The project is distributed WITHOUT ANY WARRANTY;
+    any later version. The project is distributed WITHOUT ANY WARRANTY;
     without even the implied warranty of MERCHANTABILITY or FITNESS
-    FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+    FOR A PARTICULAR PURPOSE. See the GNU GPL for more details.
 
     As additional permission under GNU GPL version 3 section 7, you
     may distribute non-source (e.g., minimized or compacted) forms of
